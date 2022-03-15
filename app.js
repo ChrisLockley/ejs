b27dec368c0ab80ef60cb41f770bfef06c65dce8
Christian <christianlockley4@gmail.com>
2022-03-15 16:02:55 -0700

    made progress on assignment

--- app.js
+++ app.js
@@ -3,3 +3,22 @@
 //in your package.json, change "main": "index.js" to "main": "app.js"
 //create a basic server using const express, app.get, and app.listen
 //use res.send to send "Welcome"
+
+const express = require('express');
+const appp = express();
+
+let ejs = require('ejs');
+
+appp.get('/', function(req, res) {
+    res.render("homepage.ejs");
+})
+
+appp.get("/game", function(req, res) {
+    res.render("game.ejs");
+})
+
+appp.set('view engine', 'ejs');
+
+appp.listen("3000", function() {
+    console.log("Website is now online");
+})
